20e27835445fb338c1fe7b82a210ed9b035e0dfa
othree <othree@gmail.com>
2015-04-27 12:11:39 +0800

    Parse constructors

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+"use strict";
+
+
 var WebIDL2 = require("webidl2");
 var fs = require('fs');
 
@@ -5,4 +8,40 @@ var file = process.argv[2];
 
 var tree = WebIDL2.parse(fs.readFileSync(file, 'utf8'));
 
-console.log(tree);
+
+var memberTraversal = function (node, fn) {
+  for (let member of node.members) {
+    fn(member, node);
+  }
+};
+
+var exposedTo = function (node) {
+  if (node.extAttrs) {
+    for (let ext of node.extAttrs) {
+      if (ext.name === 'Exposed') {
+        return ext.rhs;
+      }
+    }
+  }
+
+  return [];
+}
+
+var exposed;
+
+for (let def of tree) {
+  if (def.type === 'interface') {
+    console.log(def.name);
+    exposed = exposedTo(def);
+    if (exposed.length) {
+      console.log(` Exposed To: ${JSON.stringify(exposed)}`);
+    }
+    memberTraversal(def, function (member) {
+      if (member.type === 'operation') {
+        console.log(`  method: ${member.name}`)
+      } else {
+        console.log(`  attrib: ${member.name}`)
+      }
+    });
+  }
+}
---
+++ webidl2json.js
@@ -0,0 +1,12 @@
+"use strict";
+
+
+var WebIDL2 = require("webidl2");
+var fs = require('fs');
+
+var file = process.argv[2];
+
+var tree = WebIDL2.parse(fs.readFileSync(file, 'utf8'));
+
+
+console.log(JSON.stringify(tree));
