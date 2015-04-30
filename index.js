5e053afd59a6b761a7fe82bf356342bd36eb20e5
othree <othree@gmail.com>
2015-04-30 15:34:40 +0800

    Remove old code

--- index.js
+++ index.js
@@ -64,27 +64,3 @@ walker.on('end', function() {
   }
 });
 
-
-// var tree = WebIDL2.parse(fs.readFileSync(file, 'utf8'));
-
-
-
-// var exposed;
-
-// for (let def of tree) {
-  // if (def.type === 'interface') {
-    // console.log(JSON.stringify(factory.object(def)));
-    // console.log(def.name);
-    // exposed = exposedTo(def);
-    // if (exposed.length) {
-      // console.log(` Exposed To: ${JSON.stringify(exposed)}`);
-    // }
-    // memberTraversal(def, function (member) {
-      // if (member.type === 'operation') {
-        // console.log(`  method: ${member.name}`)
-      // } else {
-        // console.log(`  attrib: ${member.name}`)
-      // }
-    // });
-  // }
-// }
