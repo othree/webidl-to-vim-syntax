b57d893cbcba25024861b0d4ac9eca354aab79a4
othree <othree@gmail.com>
2015-05-05 17:57:58 +0800

    Generator without implements feature

--- generator.js
+++ generator.js
@@ -1,43 +1,61 @@
 /*
  *
- * primaryGlobal: {
- *   memberName: {
+ * [
+ *   {
+ *     name: 'name',
  *     type: 'cons' or 'method' or 'prop',
- *     members: {
- *       memberName: {
+ *     members: [
+ *       {
+ *         name: 'name',
  *         type: 'method' or 'prop'
  *       }
- *     }
+ *     ]
  *   }
- * }
+ * ]
  *
  */
 var generator = {
   constructor: function (def) {
+    return {
+      name: def.name,
+      type: 'cons',
+      members: generator.members(def.members)
+    }
   },
   object: function (def) {
+    return {
+      name: def.name,
+      type: 'prop',
+      members: generator.members(def.members)
+    }
   },
   implements: function (target, fromInterfaces) {
   },
+  mambers: function (members) {
+    return members.map(function (member) {
+      return {
+        name: member.name,
+        type: (member.type === 'operation') ? 'method' : 'prop'
+      };
+    });
+  }
   gen: function (st) {
-    var primaryGlobal = {};
+    var primaryGlobal = [];
 
     for (let name in st.interfaces) {
       let o = null;
-      let cons = false;
       let currentInterface = st.interfaces[name];
-      if (currentInterface.primary) {
-      } else if (currentInterface.constructor) {
+      if (currentInterface.constructor) {
         o = generator.constructor(currentInterface);
-        cons = true;
       } else if (currentInterface.named) {
         o = generator.constructor(currentInterface);
-        cons = true;
       } else if (currentInterface.nointerface) {
         continue;
       } else {
         o = generator.object(currentInterface);
       }
+
+      primaryGlobal.push(o);
     }
 
     return primaryGlobal;
