25a1f74f3a686f5b68ca8dcf8d7326c033a6391d
othree <othree@gmail.com>
2015-05-06 01:56:20 +0800

    Fix generator bugs

--- generator.js
+++ generator.js
@@ -33,22 +33,27 @@ var generator = {
     target.members = target.members.concat(generator.members(fromInterfaces.members));
   },
   implements: function (target, from, st) {
-    generator.assignMembers(target, st.interfaces[from]);
+    "use strict";
+    if (st.interfaces[from]) {
+      generator.assignMembers(target, st.interfaces[from]);
+    }
     if (st.implementations[from]) {
       for (let from of st.implementations[from]) {
         generator.implements(target, from, st);
       }
     }
   },
-  mambers: function (members) {
+  members: function (members) {
+    "use strict";
     return members.map(function (member) {
       return {
         name: member.name,
         type: (member.type === 'operation') ? 'method' : 'prop'
       };
     });
-  }
+  },
   gen: function (st) {
+    "use strict";
     var primaryGlobal = [];
 
     for (let name in st.interfaces) {
@@ -65,7 +70,7 @@ var generator = {
       }
       if (st.implementations[name]) {
         for (let from of st.implementations[name]) {
-          generator.implements(o, from);
+          generator.implements(o, from, st);
         }
       }
 
@@ -73,7 +78,7 @@ var generator = {
     }
 
     return primaryGlobal;
-  };
+  }
 };
 
 module.exports = generator;
