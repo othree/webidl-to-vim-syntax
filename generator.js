be2a5792b0300694b72f048dab00a52f173b6eed
othree <othree@gmail.com>
2015-05-08 20:41:38 +0800

    Output expected data

--- generator.js
+++ generator.js
@@ -3,7 +3,13 @@ var util = require('util');
 
 var generator = {
   run: function (data) {
-    console.log(util.inspect(data, {showHidden: false, depth: null}));
+    "use strict";
+    for (let o of data) {
+      console.log(`${o.name}: ${o.type}`);
+      for (let m of o.members) {
+        console.log(`  ${m.name}: ${m.type}`);
+      }
+    }
   }
 };
 
