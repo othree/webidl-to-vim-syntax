7590c1ef55fafa8cc89aa93ec8421bc56cfd5a1c
othree <othree@gmail.com>
2015-05-10 08:54:18 +0800

    Export global objs

--- generator.js
+++ generator.js
@@ -5,9 +5,13 @@ var generator = {
   run: function (data) {
     "use strict";
     for (let o of data) {
-      console.log(`${o.name}: ${o.type}`);
+      if (o.type === 'cons') {
+        console.log(`syntax keyword javascriptGlobal ${o.name}`);
+      } else {
+        console.log(`syntax keyword javascriptGlobal ${o.name} nextgroup=javascript${o.name}Dot`);
+        console.log(`syntax match   javascriptGlobal${o.name}Dot /\./ contained nextgroup=javascript${o.name}Method`);
+      }
       for (let m of o.members) {
-        console.log(`  ${m.name}: ${m.type}`);
       }
     }
   }
