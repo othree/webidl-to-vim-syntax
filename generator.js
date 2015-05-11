4636454dd7d4feda8f5b531478f6783b0e3f1441
othree <othree@gmail.com>
2015-05-11 11:06:32 +0800

    Next group contains prop

--- generator.js
+++ generator.js
@@ -5,13 +5,32 @@ var generator = {
   run: function (data) {
     "use strict";
     for (let o of data) {
+      let ms = false;
+      let ps = false;
+      let props = [];
+      let methods = [];
+      for (let m of o.members) {
+        if (m.type === 'prop') {
+          props.push(m.name);
+          ps = true;
+        } 
+        if (m.type === 'method') {
+          methods.push(m.name);
+          ms = true;
+        }
+      }
       if (o.type === 'cons') {
         console.log(`syntax keyword javascriptGlobal ${o.name}`);
       } else {
         console.log(`syntax keyword javascriptGlobal ${o.name} nextgroup=javascript${o.name}Dot`);
-        console.log(`syntax match   javascriptGlobal${o.name}Dot /\./ contained nextgroup=javascript${o.name}Method`);
-      }
-      for (let m of o.members) {
+        let next = 'nextgroup=';
+        if (ms || ps) {
+          let ns = [];
+          if (ms) { ns.push(`javascript${o.name}Method`); }
+          if (ps) { ns.push(`javascript${o.name}Prop`); }
+          next += ns.join(',');
+        }
+        console.log(`syntax match   javascriptGlobal${o.name}Dot /\./ contained ${next}`);
       }
     }
   }
